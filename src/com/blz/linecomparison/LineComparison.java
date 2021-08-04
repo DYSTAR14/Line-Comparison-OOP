5d66b8d06b3ab4e9c769904492b0f7b43fd787d4
DYSTAR14 <ydeepak141097@gmail.com>
2021-08-04 12:14:43 +0530

    Add:UC1 Calculate Length of Co-ordinates

--- src/com/blz/linecomparison/LineComparison.java
+++ src/com/blz/linecomparison/LineComparison.java
@@ -1,12 +1,22 @@
 package com.blz.linecomparison;
 
 public class LineComparison {
-	public void print() {
-		System.out.println("Welcome to Line Comparison Computation");
+	private final int x1,x2,y1,y2;
+	public LineComparison(int x1,int x2,int y1,int y2) {
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;	
+	}
+	public float calciLength() {
+		float length=(float) ((Math.pow((x2-x1),2) + (Math.pow((y2-y1),2))));
+		length=(float) Math.sqrt(length);
+		return length;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		LineComparison line=new LineComparison();
-		line.print();
+		System.out.println("Welcome to Line Comparison Computation");
+		LineComparison length=new LineComparison(3,-2,7,15);
+		System.out.println(length.calciLength());
 	}
 }
