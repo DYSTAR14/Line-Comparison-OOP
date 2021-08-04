5bdd4487462e3b7ac86aaefcb230f0d56ef15f5d
DYSTAR14 <ydeepak141097@gmail.com>
2021-08-04 11:46:57 +0530

    Add: Files of Line Comparison

---
+++ src/com/blz/linecomparison/LineComparison.java
@@ -0,0 +1,12 @@
+package com.blz.linecomparison;
+
+public class LineComparison {
+	public void print() {
+		System.out.println("Welcome to Line Comparison Computation");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LineComparison line=new LineComparison();
+		line.print();
+	}
+}
